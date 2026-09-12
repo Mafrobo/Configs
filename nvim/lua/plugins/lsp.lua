ad4d2001f2a3fd44c6025d3f1c5be22a7dd9673f
Mattis Bonfico <mattis.bonfico@t-online.de>
2026-09-12 09:59:43 +0200

    diagonstics

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,4 +1,16 @@
 vim.g.coq_settings = {}
 local coq = require("coq")
 vim.lsp.protocol.make_client_capabilities()
+
+vim.lsp.config('pylsp', {
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = { enabled = true },
+        pyflakes = { enabled = true },
+      }
+    }
+  }
+})
+
 vim.lsp.enable('pylsp')
